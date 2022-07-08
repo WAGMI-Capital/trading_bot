e5119ea558c4b3e58369a6d9623b20463e934b71
prampey <prudhvi.rampey@gmail.com>
2022-07-08 15:44:15 +0530

    Disocrd notify bugfix

--- utils/discord_api.py
+++ utils/discord_api.py
@@ -8,4 +8,6 @@ HOOK_URL = os.getenv('WEBHOOK_URL')
 webhook = Webhook.from_url(HOOK_URL, adapter=RequestsWebhookAdapter()) # Initializing webhook
 
 def notify_discord(msg):
+    # Maximum of 2000 characters per Discord message
+    msg = msg[:2000] if len(msg) > 2000 else msg
     webhook.send(msg)
